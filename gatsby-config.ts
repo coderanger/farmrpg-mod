080acbd05319e6caea6e0204641a932a13aaa910
Noah Kantrowitz <noah@coderanger.net>
2022-04-17 15:35:54 -0700

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,16 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `farmrpg-mod`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-emotion", {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": ""
+    }
+  }, "gatsby-plugin-react-helmet"]
+};
+
+export default config;
